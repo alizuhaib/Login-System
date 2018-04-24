2d40acddd489e506b647223ff812c2cb2aeccdf3
Ali Zohaib <ali@ad-4.local>
2018-04-24 18:47:36 +0500

    adding all the files...

--- login_app.py
+++ login_app.py
@@ -1,29 +1,24 @@
-"""
-login app"
-"""
-from flask import Flask, session, redirect, url_for, escape, request, render_template, jsonify
-from hashlib import md5
-import MySQLdb
-from flask_restful import reqparse, abort, Api, Resource
+from flask import Flask, session, request
+from flask_restful import Api, Resource
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 api = Api(app)
+mysql = MySQL()
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="", db="new_schem")
-cur = db.cursor()
-
-
-def logout():
-    session.pop('UserName', None)
-    return print("User is now logout from the device")
-
-
-# @app.route('/login', methods=['GET', 'POST'])
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'new_login'
+app.config['MYSQL_DATABASE_HOST'] = ''
+mysql.init_app(app)
 
 
 class LoginUser(Resource):
-
     def post(self):
+        """"From this function, User is log into the device, If user is not registered, first he had to sign up"""
+
+        conn = mysql.get_db()
+        cursor = conn.cursor()
         error = None
 
         if 'UserName' in session:
@@ -33,11 +28,11 @@ class LoginUser(Resource):
             username_form = request.form['UserName']
             password_secret = request.form['Pwd']
 
-            cur.execute("SELECT Count(1) FROM login WHERE UserName = %s;", [username_form])
-            if cur.fetchone()[0]:
+            cursor.execute("SELECT Count(1) FROM login WHERE UserName = %s;", [username_form])
+            if cursor.fetchone()[0]:
 
-                cur.execute("SELECT Pwd FROM login WHERE UserName = %s;", [username_form])
-                for row in cur.fetchall():
+                cursor.execute("SELECT Pwd FROM login WHERE UserName = %s;", [username_form])
+                for row in cursor.fetchall():
 
                     # if md5(password_secret).hexdigest() == row[0]:
                     if password_secret == row[0]:
@@ -53,36 +48,76 @@ class LoginUser(Resource):
 
 
 class AddUser(Resource):
-
     def post(self):
+        """"Sign up function"""
+        conn = mysql.get_db()
+        # conn = mysql.connect()
+        cursor = conn.cursor()
 
         username_form = request.form['UserName']
         pwd_form = request.form['Pwd']
-        cur.execute("Select Count(1) FROM login WHERE UserName = %s", [username_form])
-        if cur.fetchone()[0]:
+        cursor.execute("Select Count(1) FROM login WHERE UserName = %s", [username_form])
+        if cursor.fetchone()[0]:
             return "user is already present in the db"
         else:
-            query = ("""INSERT INTO login VALUES (%s,%s)""", (username_form, pwd_form))
-            cur.execute(query)
+            cursor.execute("INSERT INTO login (UserName, Pwd) VALUES ('%s','%s')" % (username_form, pwd_form))
+            conn.commit()
 
             return "user is successfully added into the db"
+        conn.close()
 
 
+class UpdateUser(Resource):
+    def put(self):
+        """"If you want the password or username , you can update user information from this function"""
+        conn = mysql.get_db()
+        # conn = mysql.connect()
+        cursor = conn.cursor()
+        # user_id = os.geteuid()
+        username_form = request.form['UserName']
+        pwd_form = request.form['Pwd']
+        cursor.execute("SELECT Count(1) FROM login WHERE UserName = %s;", [username_form])
+        if cursor.fetchone()[0]:
+            cursor.execute("Select Id from login WHERE UserName = %s", (username_form))
+            uid = cursor.fetchone()
+            cursor.execute("UPDATE login SET Pwd = '%s' WHERE ID = '%s'" % (pwd_form, uid[0]))
+
+            conn.commit()
+            return "user is successfully updated in the db"
+
+        else:
+            return "user is not found in the db"
 
 
-@app.route('/logout')
-def logout():
-    session.pop('UserName', None)
-    return "User is getting logout from the device"
+class DeleteUser(Resource):
+    def delete(self):
+        """If you want to delete some user, simple enter their name and the user will be deleted"""
+        conn = mysql.get_db()
+        # conn = mysql.connect()
+        cursor = conn.cursor()
+
+        username_form = request.form['UserName']
+        cursor.execute("SELECT Count(1) FROM login WHERE UserName = %s;", [username_form])
+
+        if cursor.fetchone()[0]:
+            cursor.execute("Select Id from login WHERE UserName = %s", [username_form])
+            uid = cursor.fetchone()
+            cursor.execute("DELETE from login WHERE Id = %s", (uid[0]))
+            conn.commit()
+            return "User is successfully removed from db"
+            # cursor.execute("DELETE from login WHERE UserName = 'aslam'")
+        else:
+            return "User does not exist"
 
 
-api.add_resource(AddUser, '/adduser')
-api.add_resource(LoginUser, '/login')
-
+api.add_resource(UpdateUser, '/updateuser')
+# api.add_resource(LoginUser, '/login')
+# api.add_resource(DeleteUser, '/deluser')
+# api.add_resource(AddUser, '/adduser')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
 
 
 
